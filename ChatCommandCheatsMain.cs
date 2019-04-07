f601fae5214477a2a6e7ccee80cb5d3c9fad4228
Frogtown Matt <frogtownmatt@gmail.com>
2019-04-07 10:29:12 -0500

    Change to be just cheat commands

---
+++ ChatCommandCheatsMain.cs
@@ -0,0 +1,64 @@
+﻿using RoR2;
+using System;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace Frogtown
+{
+    public class ChatCommandCheatsMain
+    {
+        public static bool enabled;
+        public static UnityModManager.ModEntry modEntry;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            ChatCommandCheatsMain.modEntry = modEntry;
+            modEntry.OnToggle = OnToggle;
+            FrogtownShared.AddChatCommand("char", OnCharCommand);
+            enabled = true;
+            return true;
+        }
+        
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            FrogtownShared.ModToggled(value);
+            return true;
+        }
+
+        private static bool OnCharCommand(string userName, string[] pieces)
+        {
+            if (!enabled)
+            {
+                return false;
+            }
+
+            int prefabIndex = -1;
+            if (!Int32.TryParse(pieces[1], out prefabIndex))
+            {
+                prefabIndex = BodyCatalog.FindBodyIndexCaseInsensitive(pieces[1]);
+            }
+            if (prefabIndex != -1)
+            {
+                GameObject prefab = BodyCatalog.GetBodyPrefab(prefabIndex);
+
+                if (prefab != null)
+                {
+                    if(FrogtownShared.ChangePrefab(userName, prefab))
+                    {
+                        FrogtownShared.SendChat(userName + " morphed into " + prefab.name);
+                    }
+                    else
+                    {
+                        FrogtownShared.SendChat(userName + " couldn't morph into " + prefab.name);
+                    }
+                }
+                else
+                {
+                    FrogtownShared.SendChat("Prefab not found");
+                }
+            }
+            return true;
+        }
+    }
+}
